af832a02bafb8a1d015518b6359bd9018ae35d0e
Krish Advani <krish@quickwork.co>
2018-08-15 20:14:21 +0530

    message

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var Gpio = require('pigpio').Gpio;
-const led = new Gpio(25, {mode: Gpio.OUTPUT});
+const led = new Gpio(13, {mode: Gpio.OUTPUT});
 var dutyCycle = process.argv.splice(2);
 console.log("Speed Passed:" + dutyCycle);
 if(dutyCycle < 255){
