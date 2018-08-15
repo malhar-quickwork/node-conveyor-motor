9652701ff6d9f95a773f33a19bb32e43277b52e4
Krish Advani <krish@quickwork.co>
2018-08-15 20:06:31 +0530

    message

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 var Gpio = require('pigpio').Gpio;
 const led = new Gpio(25, {mode: Gpio.OUTPUT});
-let dutyCycle = 50;
-var args = process.argv.splice(2);
-console.log("Arguments:" + args);
-led.pwmWrite(args[0]);
+var dutyCycle = process.argv.splice(2);
+console.log("Speed Passed:" + dutyCycle);
+if(dutyCycle < 255){
+    led.pwmWrite(dutyCycle);
+}
 process.on('SIGINT', function() {
 	led.digitalWrite(0);
 	Gpio.terminate();
