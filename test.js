8511e57583333f75230f814b91652d2956eb59e9
Krish Advani <krish@quickwork.co>
2018-08-15 20:22:38 +0530

    message

--- test.js
+++ test.js
@@ -5,3 +5,19 @@ console.log("Speed Passed:" + dutyCycle);
 if(dutyCycle < 255){
     led.pwmWrite(dutyCycle);
 }
+dutyCycle = 0;
+int i = 0;
+while(true){
+	dutyCycle = dutyCycle + i;
+	console.log("DUTY CYCLE" + dutyCycle);
+    led.pwmWrite(dutyCycle);
+    i++;
+
+    if(i >= 100){
+    	dutyCycle = 0;
+    	i = 0;
+    }
+    setTimeout(function() {
+    	console.log('Blah blah blah blah extra-blah');
+	}, 3000);
+}
