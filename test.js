17eb8cf468ad88d2d458b053d0f8f48a4e4ae505
Krish Advani <krish@quickwork.co>
2018-08-15 20:05:40 +0530

    message

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var Gpio = require('pigpio').Gpio;
 const led = new Gpio(25, {mode: Gpio.OUTPUT});
 let dutyCycle = 50;
-var args = process.argv;
+var args = process.argv.splice(2);
 console.log("Arguments:" + args);
 led.pwmWrite(args[0]);
 process.on('SIGINT', function() {
