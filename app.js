08dea7559456c4787fb86809338af4382c2c516c
malhar <malhar@quickwork.co>
2018-08-15 18:04:44 +0530

    Some changes

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 var http = require('http');
 var Gpio = require('pigpio').Gpio,
 motorOut = new Gpio(22, {mode: Gpio.OUTPUT});
+runMotor();
+function runMotor() {
+    motorOut.digitalWrite(1);
+}
+function stopMotor() {
+    motorOut.digitalWrite(0);
+}
+setTimeout(stopMotor,10000);
