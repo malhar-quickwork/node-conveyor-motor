1d0e7846661a06c4a3a694dd1aa81229781f0893
malhar <malhar@quickwork.co>
2018-08-16 18:48:19 +0530

    sdas

--- brainfulDrone/server/modules/PCA9685-i2cMotorInterface.js
+++ brainfulDrone/server/modules/PCA9685-i2cMotorInterface.js
@@ -1,9 +1,7 @@
 const config = require('./config.js');
 const raspi = require('raspi');
 const PWM = require('raspi-pwm').PWM;
-const Gpio = require('pigpio').Gpio;
-const led = new Gpio(13, {mode: Gpio.OUTPUT});
-let pwm12,pwm13,pwm17;
+let pwm12,pwm13,pwm18;
 
 let self ={
     auto : false,
@@ -12,19 +10,17 @@ let self ={
     settings :{ kickUpTick : 0 , kickDownTickMin : 40 , kickDownTickMax : 600 },
     wire : null,
     init : (next)=>{ 
-        led.digitalWrite(0);
-        led.digitalWrite(1);
-        /* raspi.init(() => {
+        raspi.init(() => {
             pwm12 = new PWM('GPIO12');
             pwm13 = new PWM('GPIO13');
-            pwm17 = new PWM('GPIO17');
+            pwm17 = new PWM('GPIO18');
             console.log(pwm12);
             console.log(pwm13);
-            console.log(pwm17);
-        }); */
+            console.log(pwm18);
+        });
         next();
     },
-    motorsArr : [pwm13,pwm12,pwm17],
+    motorsArr : [pwm13,pwm12,pwm18],
     testInit:(allOne,next)=>{
     },
     haltAll:(next)=>{
@@ -38,11 +34,12 @@ let self ={
 		if(speed < 130){
             console.log('Speed On GPIO' + self.motors[selected-1] + ' : ',speed);
             console.log(self.motorsArr);
+            console.log(pwm13);
+            console.log(pwm+self.motors[selected-1]);
             for (let i = 0; i < self.motorsArr.length; i++){
                 console.log(i);
                 console.log(self.motorsArr[i]);
             }
-            led.pwmWrite(speed);
 		    //self.motorsArr[selected-1].write(speed);
 		    console.log('Speed On GPIO' + self.motors[selected-1] + ' : ',speed);
 	    }
