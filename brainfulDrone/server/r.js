a3026340794098a3bf582bcfa6b49b8563fcee8e
Krish Advani <krish@quickwork.co>
2018-08-16 13:39:18 +0530

    as

--- brainfulDrone/server/r.js
+++ brainfulDrone/server/r.js
@@ -11,24 +11,19 @@ const config = require('./modules/config.js');
 const mocks = require('./modules/mocks.nonPi.js');
 
 let motor = config.isPi? require('./modules/PCA9685-i2cMotorInterface.js'):mocks.motor;
-let gyro = config.isPi? require('./modules/gyro-shim.js'):mocks.gyro;
-let camera = config.isPi? require('./../cam/camstreamer.js'):null;
 let socketHandleV3 =  require('./modules/v3.js') ;
 
 let currentHandler = socketHandleV3 ;
 currentHandler.setReferences(io,motor,gyro, function(){
-    camera.start(CAM_PORT);
 });
 
 motor.init(()=>{
-    camera.startFailSafe(CAM_PORT,1);
     config.camStreamSrc = 'http://'+ip.address()+':'+CAM_PORT+'/?action=stream';
         console.log('Belt at : '+config.camStreamSrc);
     
     app.use(express.static(__dirname + '/public'));  
     server.listen(PORT,()=>{
         console.log('Belt Server: http://'+ip.address()+':'+PORT);    
-        gyro.init();
         io.on('connection', currentHandler.socketHandle);
     });
 });
