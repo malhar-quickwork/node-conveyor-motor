5211a986fb67a3f9213e14457fee810f8816cdba
Krish Advani <krish@quickwork.co>
2018-08-16 14:35:07 +0530

    as

--- brainfulDrone/server/modules/PCA9685-i2cMotorInterface.js
+++ brainfulDrone/server/modules/PCA9685-i2cMotorInterface.js
@@ -1,10 +1,13 @@
 const config = require('./config.js');
 Gpio = require('pigpio').Gpio;
 motor13 = new Gpio(13, {mode: Gpio.OUTPUT});
+motor12 = new Gpio(12, {mode: Gpio.OUTPUT});
+motor18 = new Gpio(18, {mode: Gpio.OUTPUT});
 let self ={
     auto : false,
     address : config.motorAddress ,
-    motors : [6,7,9,10],
+    motors : [13,12,17],
+    motorsArr : [motor13,motor12,motor17,motor18],
     settings :{ kickUpTick : 0 , kickDownTickMin : 40 , kickDownTickMax : 600 },
     wire : null,
     init : (next)=>{ 
@@ -15,14 +18,21 @@ let self ={
     },
     haltAll:(next)=>{
 
+        for(let i = 0 ; i < self.motors.length; i++){
+            self.motorsArr[i].pwmWrite(30);
+        }
+
     },
     throttle:(selected,speed,next)=>{
-        motor13.pwmWrite(speed);
+        self.motorsArr[selected].pwmWrite(speed);
     },
     multiThrottle:(selected,speeds,next)=>{
 
     },    
     allThrottle:(speed,next)=>{
+        for(let i = 0 ; i < self.motors.length; i++){
+            self.motorsArr[i].pwmWrite(speed);
+        }
     },    
 }
 
