d2ad688251eb3f5378e97f0b58d92504ae33a5bb
malhar <malhar@quickwork.co>
2018-08-17 13:01:31 +0530

    start stop speed

--- brainfulDrone/server/r.js
+++ brainfulDrone/server/r.js
@@ -57,7 +57,7 @@ motor.init(()=>{
             default:
                 break;
         }
-        res.send(200,motorName);
+        res.send(200,{'motor':motorName,'speed':0});
     });
     app.route('/api/motor/:motor/speed/:speed').get((req,res) => {
         let motorName = req.params['motor'];
@@ -85,6 +85,34 @@ motor.init(()=>{
             default:
                 break;
         }
-        res.send(200,motorName);
+        res.send(200,{'motor':motorName,'speed':speed});
+    });
+    app.route('/api/motor/:motor/start').get((req,res) => {
+        let motorName = req.params['motor'];
+        let speed = 66;
+        console.log('REQUEST '+motorName+' speed '+speed);
+        switch (motorName) {
+            case 'motor1':    
+            socketClient.emit('speed-motor', {payload:{motorNumber : 1, value : speed},message:'speed change'});
+                break;
+            case 'motor2':
+            socketClient.emit('speed-motor', {payload:{motorNumber : 2, value : speed},message:'speed change'});
+                break;
+            case 'motor3':
+            socketClient.emit('speed-motor', {payload:{motorNumber : 3, value :speed},message:'speed change'});
+                break;
+            case 'conveyor1':
+            socketClient.emit('speed-dual-motor', {payload:{m1 : 2, m2 : 3, value : speed},message:'speed change'});
+                break;
+            case 'conveyor2':
+            socketClient.emit('speed-motor', {payload:{motorNumber : 1, value : speed},message:'speed change'});
+                break;
+            case 'all':
+                socketClient.emit('speed-all-motor',  {payload:{ value : speed},message:'speed change all'});
+                break;
+            default:
+                break;
+        }
+        res.send(200,{'motor':motorName,'speed':speed});
     });
 });
