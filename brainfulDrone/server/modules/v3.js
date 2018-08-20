b1522028647d6c8696bb851fe6e3767025362889
malhar <malhar@quickwork.co>
2018-08-20 16:02:42 +0530

    erfsd

--- brainfulDrone/server/modules/v3.js
+++ brainfulDrone/server/modules/v3.js
@@ -142,7 +142,7 @@ module.exports =  {
             var postData  = automationconfig.form;
             if(data.payload && data.payload.event){
                 postData.payload.type = data.payload.event.toUpperCase();
-                postData.initialData = JSON.stringify(data.payload);
+                postData.initialData = data.payload;
                /*  switch(data.payload.event) {
                     case 'speed_fail' : 
                     
